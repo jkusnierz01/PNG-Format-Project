62a92cf89870e7bef83030f4da357e223c5a823e
jkusnierz01 <jedrek33@onet.pl>
2024-06-09 18:44:27 +0200

    ECB encryption fail

--- e-media1/e_media1/encrypt.py
+++ e-media1/e_media1/encrypt.py
@@ -24,25 +24,21 @@ class RSA:
         
 
     
-    
-    
 
-    def x(self):
-        print(self.public_key.public_numbers())
-
-    
-
-
+@dataclass
 class ECB():
+    key: bytes = None
+    added_bytes: int = None
+    _encrypted: np.array = None
+    _original: np.array = None
 
-    def __init__(self) -> None:
-        self.generateECBkey()
 
-    @classmethod
-    def generateECBkey(cls ,number_of_bytes: int = 12):
+    def __post_init__(self) -> None:
+        self.generatekey()
+
+    def generatekey(self ,number_of_bytes: int = 12):
         logger.info("Generating ECB key")
-        cls.key = os.urandom(number_of_bytes)
-
+        self.key = os.urandom(number_of_bytes)
 
     '''
     Funkcja do dzielenia dane z obrazu na bloki. Deafaultowo 12 bajtowe. Dla obrazu RGB będą to 4 piksele.
@@ -51,8 +47,7 @@ class ECB():
     Return:
         * 
     '''
-    @staticmethod
-    def splitData(full_data: np.array, number_of_bytes: int = 12):
+    def split_data(self, full_data: np.array, number_of_bytes: int = 12):
         logger.info("Performing IDAT Data splitting")
         #we take the original shape and create 1D vector with data
         shape_tuple = full_data.shape
@@ -72,8 +67,8 @@ class ECB():
                 diff = len(full_data) - idx
                 last_data_bytes = full_data[idx:idx+diff]
                 missing_bytes = number_of_bytes - diff
-                added_bytes = [0 for iter in range(missing_bytes)]
-                list_sum = np.concatenate((last_data_bytes,added_bytes),axis=0)
+                self.added_bytes = [0 for iter in range(missing_bytes)]
+                list_sum = np.concatenate((last_data_bytes,self.added_bytes),axis=0)
                 data_blocks.append(list_sum)
                 idx += diff
         logger.info("Data succesfully splitted")
@@ -84,24 +79,40 @@ class ECB():
 
     '''
     Funkcja przeprowadzająca szyfrowanie ECB z wykrzystaniem operacji XOR między bajtami w kluczu i danych.
-        * full_IDAT_data -> np.array: tablica zawierajaca dane IDAT z obrazu po dekompresji i odfiltrowaniu
+        * full_data -> np.array: tablica zawierajaca dane IDAT z obrazu po dekompresji i odfiltrowaniu lub dane po zaszyfrowaniu
+        Operacja XOR pozwala na to ze odszyfrowywanie odbywa sie ta sama metoda
     '''
-    def ECBencryption(self, full_IDAT_data:np.array):
-        logger.info("Starting encryption with ECB algorithm")
-        data,shape,nr_of_added_bytes = self.splitData(full_IDAT_data)
-        _encrypted = []
+
+    def encrypt_and_decrypt_algorithm(self, full_data:np.array):
+        data,shape,nr_of_added_bytes = self.split_data(full_data)
+        data_after_operation = []
         # for every byte in data block we perform XOR operation with corresponding byte in key.
         for block in data:
             for iter in range(len(block)):
-                _encrypted.append(block[iter] ^ self.key[iter])
+                data_after_operation.append(block[iter] ^ self.key[iter])
         # if we have additional encrypted data we get rid of it for now - we dont need it to recreate image
         if nr_of_added_bytes is not None:
-            _encrypted = _encrypted[:-nr_of_added_bytes]
-        _encrypted = np.reshape(_encrypted,shape)
-        logger.info("Data encrypted with ECB algorithm")
-        
+            data_after_operation = data_after_operation[:-nr_of_added_bytes]
+        data_after_operation = np.reshape(data_after_operation,shape)
+
         #we create encrypted image
-        f = plt.figure()
-        plt.imshow(_encrypted)
-        plt.axis("off")
-        plt.show()
+        # f = plt.figure()
+        # plt.imshow(data_after_operation)
+        # plt.axis("off")
+        # plt.show()
+
+        return data_after_operation
+        
+    
+    def encrypt(self,original_data):
+        logger.info("Starting encryption with ECB algorithm")
+        self._encrypted = self.encrypt_and_decrypt_algorithm(original_data)
+        logger.info("Encryption with ECB algorithm successful")
+
+    def decrypt(self):
+        logger.info("Starting decryption with ECB algorithm")
+        self._original = self.encrypt_and_decrypt_algorithm(self._encrypted)
+        logger.info("Decryption with ECB algorithm successful")
+
+
+class CTR:
